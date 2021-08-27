615f333f3681807c5d67de7be4e16436bf2af027
syafiqsaleem <19syafiq89@gmail.com>
2021-08-27 21:51:50 +0800

    Routes & Controllers creation

---
+++ routes/auth.js
@@ -0,0 +1,13 @@
+import express from "express";
+
+const router = express.Router();
+
+// comtrollers
+import { showMessage } from "../controllers/auth";
+
+router.get("/:message", showMessage);
+
+// TypeEror: Router.use() requires a middleware function but got a Object
+// Wrong way of importing and exporting
+// Since we are using require in the autoloading routes, we cannot use export default
+module.exports = router;
--- server.js
+++ server.js
@@ -1,9 +1,23 @@
 import express from "express";
+// import router from "./routes/auth" --> No longer needed due to autoloading routes
+import { readdirSync } from "fs"; // comes with node.js as default (autoloading routes)
+const morgan = require("morgan");
+require("dotenv").config();
 
 const app = express();
 
-app.get("/api/:message", (req, res) => {
-  res.status(200).send(`Here is your message: ${req.params.message}`);
-});
+// middlewares
+app.use(morgan("dev")); // In development mode(dev) --> helps tell us what kind of request was made(GET/POST/etc)
+// route middleware
+// Autoloading routes:
+// readdirSync --> To ensure we are reading all the routes
+// readdirSync takes a function of the directory that we want it to read
+// map --> to map all the files that are in the routes folder
+// map takes a function as an argument
+// r --> shortform for routes
+// When we map through the files we apply each of them as a middleware
+readdirSync("./routes").map((r) => app.use("/api", require(`./routes/${r}`)));
 
-app.listen(8000, () => console.log(`Server is running on port 8000`));
+const port = process.env.PORT || 8000;
+
+app.listen(port, () => console.log(`Server is running on port ${port}`));
