6c76eb2ca7e742cd7fabca2e1541fd614b8df1bf
syafiqsaleem <19syafiq89@gmail.com>
2021-09-02 15:58:37 +0800

    Bcrypt hash password

--- models/user.js
+++ models/user.js
@@ -1,4 +1,5 @@
 import mongoose from "mongoose";
+import bcrypt from "bcrypt";
 const { Schema } = mongoose;
 
 const userSchema = new Schema(
@@ -26,5 +27,24 @@ const userSchema = new Schema(
   },
   { timestamps: true }
 );
+
+userSchema.pre("save", function (next) {
+  let user = this;
+  // hash password only if user is changing the password/registering for the first time
+  // make sure to use this otherwise each time user.save() is executed, password
+  // will get auto updated and user cannot login with original password
+    if(user.isModified('password')) {
+        return bcrypt.hash(user.password, 12, function (err. hash) {
+            if(err) {
+                console.log('BCRYPT HASH ERR ', err)
+                return next(err);
+            }
+            user.password = hash;
+            return next();
+        });
+    } else {
+        return next();
+    }
+});
 
 export default mongoose.model("User", userSchema);
