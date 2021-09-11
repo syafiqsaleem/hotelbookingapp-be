28748fc6d804b41ac3a0c5149463d661e0714025
syafiqsaleem <19syafiq89@gmail.com>
2021-09-11 18:45:24 +0800

    create Log in link for stripe

--- controllers/stripe.js
+++ controllers/stripe.js
@@ -5,6 +5,7 @@
 
 import User from "../models/user";
 import Stripe from "stripe";
+import queryString from "query-string";
 
 const stripe = Stripe(process.env.STRIPE_SECRET);
 
@@ -21,6 +22,20 @@ export const createConnectAccount = async (req, res) => {
     user.stripe_account_id = account.id;
     user.save();
   }
-  // 3) create account link based on account id (for frontend to complete onboarding)
+  // 3) create login link based on account id (for frontend to complete onboarding)
+  let accountLink = await stripe.accountLinks.create({
+    account: user.stripe_account_id,
+    refresh_url: process.env.STRIPE_REDIRECT_URL,
+    return_url: process.env.STRIPE_REDIRECT_URL,
+    type: "account_onboarding",
+  });
+  // prefill any info such as email
+  accountLink = Object.assign(accountLink, {
+    "stripe_user[email]": user.email || undefined,
+  });
+  // console.log("ACCOUNT LINK", accountLink);
+  let link = `${accountLink.url}?${queryString.stringify(accountLink)}`;
+  console.log("LOGIN LINK", link);
+  res.send(link);
   // 4) update payment schedule (optional. default is 2 days)
 };
