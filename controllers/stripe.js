d4e410cfcf3785d37590547e7022a9b9b82f739a
syafiqsaleem <19syafiq89@gmail.com>
2021-09-11 17:35:38 +0800

    stripe backend

--- controllers/stripe.js
+++ controllers/stripe.js
@@ -1,4 +1,22 @@
+// export const createConnectAccount = async (req, res) => {
+//   console.log("REQ USER FROM REQUIRE_SIGNIN MIDDLEWARE", req.user);
+//   console.log("YOU HIT CREATE CONNECT ACCOUNT ENDPOINT");
+// };
+
+import User from "../models/user";
+import Stripe from "stripe";
+
+const stripe = Stripe(process.env.STRIPE_SECRET);
+
 export const createConnectAccount = async (req, res) => {
-  console.log("REQ USER FROM REQUIRE_SIGNIN MIDDLEWARE", req.user);
-  console.log("YOU HIT CREATE CONNECT ACCOUNT ENDPOINT");
+  // 1) find user from db
+  const user = await User.findById(req.user._id).exec();
+  console.log("USER ===> ", user);
+  // 2) if user do not have stripe_account_id yet, create now
+  const account = await stripe.accounts.create({
+    type: "express",
+  });
+  console.log("ACCOUNT ===> ", account);
+  // 3) create account link based on account id (for frontend to complete onboarding)
+  // 4) update payment schedule (optional. default is 2 days)
 };
