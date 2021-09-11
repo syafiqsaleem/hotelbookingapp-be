0853ed13aa622ab1d9bffdde08987b68ee6c2fa6
syafiqsaleem <19syafiq89@gmail.com>
2021-09-11 17:48:21 +0800

    saving generated stripe_account_id into db

--- controllers/stripe.js
+++ controllers/stripe.js
@@ -13,10 +13,14 @@ export const createConnectAccount = async (req, res) => {
   const user = await User.findById(req.user._id).exec();
   console.log("USER ===> ", user);
   // 2) if user do not have stripe_account_id yet, create now
-  const account = await stripe.accounts.create({
-    type: "express",
-  });
-  console.log("ACCOUNT ===> ", account);
+  if (!user.stripe_account_id) {
+    const account = await stripe.accounts.create({
+      type: "express",
+    });
+    console.log("ACCOUNT ===> ", account);
+    user.stripe_account_id = account.id;
+    user.save();
+  }
   // 3) create account link based on account id (for frontend to complete onboarding)
   // 4) update payment schedule (optional. default is 2 days)
 };
