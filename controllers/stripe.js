7d015b5342aa35406e2f04d604e565440bb47d3a
syafiqsaleem <19syafiq89@gmail.com>
2021-09-14 01:17:41 +0800

    Stripe Users Settings

--- controllers/stripe.js
+++ controllers/stripe.js
@@ -10,10 +10,10 @@ import queryString from "query-string";
 const stripe = Stripe(process.env.STRIPE_SECRET);
 
 export const createConnectAccount = async (req, res) => {
-  // 1) find user from db
+  // 1. find user from db
   const user = await User.findById(req.user._id).exec();
-  console.log("USER ===> ", user);
-  // 2) if user do not have stripe_account_id yet, create now
+  console.log("USER ==> ", user);
+  // 2. if user don't have stripe_account_id yet, create now
   if (!user.stripe_account_id) {
     const account = await stripe.accounts.create({
       type: "express",
@@ -22,7 +22,7 @@ export const createConnectAccount = async (req, res) => {
     user.stripe_account_id = account.id;
     user.save();
   }
-  // 3) create login link based on account id (for frontend to complete onboarding)
+  // 3. create login link based on account id (for frontend to complete onboarding)
   let accountLink = await stripe.accountLinks.create({
     account: user.stripe_account_id,
     refresh_url: process.env.STRIPE_REDIRECT_URL,
@@ -37,7 +37,7 @@ export const createConnectAccount = async (req, res) => {
   let link = `${accountLink.url}?${queryString.stringify(accountLink)}`;
   console.log("LOGIN LINK", link);
   res.send(link);
-  // 4) update payment schedule (optional. default is 2 days)
+  // 4. update payment schedule (optional. default is 2 days
 };
 
 const updateDelayDays = async (accountId) => {
@@ -80,9 +80,26 @@ export const getAccountBalance = async (req, res) => {
     const balance = await stripe.balance.retrieve({
       stripeAccount: user.stripe_account_id,
     });
-    // console.log("BALANCE ===> ", balance);
+    // console.log("BALANCE ===>", balance);
     res.json(balance);
   } catch (err) {
     console.log(err);
+  }
+};
+
+export const payoutSetting = async (req, res) => {
+  try {
+    const user = await User.findById(req.user._id).exec();
+
+    const loginLink = await stripe.accounts.createLoginLink(
+      user.stripe_account_id,
+      {
+        redirect_url: process.env.STRIPE_SETTING_REDIRECT_URL,
+      }
+    );
+    // console.log("LOGIN LINK FOR PAYOUT SETTING", loginLink);
+    res.json(loginLink);
+  } catch (err) {
+    console.log("STRIPE PAYOUT SETTING ERR ", err);
   }
 };
